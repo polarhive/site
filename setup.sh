1a50d15c318294b8111499e3b813b776cd9a5db1
Nathan Paul <mail@polarhive.net>
2024-04-16 04:36:55 +0530

    migrate/anna: integrate wiki ref: #1

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+hugo
+git clone https://github.com/polarhive/knowledge --depth=1; cd knowledge || exit
+curl -L https://github.com/acmpesuecc/anna/releases/download/v2.0.0/anna_Linux_x86_64.tar.gz > anna_Linux_x86.tar.gz; tar -xvzf anna_Linux_x86.tar.gz; 
+./anna
+mv -f site/rendered/static ../public/static
+mv -f site/rendered/wiki ../public/wiki
