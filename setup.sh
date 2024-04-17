15e536f5527ffed477c8ab76d12d22f0fb29b8f7
Nathan Paul <mail@polarhive.net>
2024-04-17 19:47:29 +0530

    fix: rm old wiki

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 hugo
+rm -rf knowledge
 git clone https://github.com/polarhive/knowledge --depth=1; cd knowledge || exit
 curl -L https://github.com/acmpesuecc/anna/releases/download/v2.0.0/anna_Linux_x86_64.tar.gz > anna_Linux_x86.tar.gz; tar -xvzf anna_Linux_x86.tar.gz; 
 ./anna
