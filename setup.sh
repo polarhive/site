41eab33418994445da51a2a57cbf3d09e3d60d19
Nathan Paul <mail@polarhive.net>
2024-06-24 19:36:31 +0530

    chore: update anna path

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 hugo
 rm -rf knowledge
 git clone https://github.com/polarhive/knowledge --depth=1; cd knowledge || exit
-curl -L https://github.com/acmpesuecc/anna/releases/download/v2.0.0/anna_Linux_x86_64.tar.gz > anna_Linux_x86.tar.gz; tar -xvzf anna_Linux_x86.tar.gz; 
+curl -L https://github.com/anna-ssg/anna/releases/download/v2.0.0/anna_Linux_x86_64.tar.gz > anna_Linux_x86.tar.gz; tar -xvzf anna_Linux_x86.tar.gz;
 ./anna
 mv -f site/rendered/static ../public/static
 mv -f site/rendered/wiki ../public/wiki
