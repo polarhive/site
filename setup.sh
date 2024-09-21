2b57de7ffc3eef1a9f22c95475269d20702fcb25
Nathan Paul <mail@polarhive.net>
2024-09-21 16:39:38 +0530

    wiki: UE23CS

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 hugo
 rm -rf knowledge
 git clone https://github.com/polarhive/knowledge --depth=1
-cd knowledge || exit
-mv README.md content/index.md
+git clone https://github.com/polarhive/UE23CS --depth=1
+mv UE23CS knowledge/content/uni
 npm i
 npx quartz build
 mv -f public ../public/wiki
