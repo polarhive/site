21a4dfe3f4fd5f289cd89b704c9e97b7793175ca
Nathan Paul <mail@polarhive.net>
2024-09-22 01:49:29 +0530

    wiki: merge content

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 hugo
 rm -rf knowledge UE23CS
 git clone https://github.com/polarhive/knowledge --depth=1
-git clone https://github.com/polarhive/UE23CS --depth=1 knowledge/content/uni
+git clone https://github.com/polarhive/UE23CS --depth=1 knowledge/quartz/content/uni
 cd knowledge/quartz || exit
-mv ../content .
 npm i
 npx quartz build
 mv -f public ../../public/wiki
