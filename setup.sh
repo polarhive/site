973ec68a6899209d8640dcb6ae71bb9538f123b0
Nathan Paul <mail@polarhive.net>
2024-09-21 17:02:17 +0530

    wiki: right dirs

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ hugo
 rm -rf knowledge UE23CS
 git clone https://github.com/polarhive/knowledge --depth=1
 git clone https://github.com/polarhive/UE23CS --depth=1 knowledge/content/uni
-cd knowledge || exit
+cd knowledge/quartz || exit
+mv ../content .
 npm i
 npx quartz build
-mv -f public ../public/wiki
+mv -f public ../../public/wiki
