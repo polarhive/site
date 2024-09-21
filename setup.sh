4a766574855c0a906011a536bdee83c0c3b51046
Nathan Paul <mail@polarhive.net>
2024-09-21 16:08:09 +0530

    fix: npm

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ rm -rf knowledge
 git clone https://github.com/polarhive/knowledge --depth=1
 cd knowledge || exit
 mv README.md content/index.md
+npm i
 npx quartz build
 mv -f public ../public/wiki
