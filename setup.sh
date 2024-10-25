7224038293c620607d0b38d13eff516acf2a6193
Nathan Paul <mail@polarhive.net>
2024-10-25 23:49:27 +0530

    fix: new path

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 hugo
-rm -rf knowledge UE23CS
+rm -rf knowledge UE23CS /tmp/UE23CS
 git clone https://github.com/polarhive/knowledge --depth=1
-git clone https://github.com/polarhive/UE23CS --depth=1 knowledge/quartz/content/uni
+git clone --depth=1 https://github.com/polarhive/UE23CS /tmp/UE23CS
+mv /tmp/UE23CS/src knowledge/quartz/content/uni/
+mv /tmp/UE23CS/README.md knowledge/quartz/content/uni/index.md
 cd knowledge/quartz || exit
-npm i
-npx quartz build
+npm i; npx quartz build
 mv -f public ../../public/wiki
