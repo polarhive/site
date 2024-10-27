76fe3bbe21e274323e0c7a381e44404d96c9433e
Nathan Paul <mail@polarhive.net>
2024-10-27 15:18:33 +0530

    rename: wiki

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-hugo
-rm -rf knowledge UE23CS /tmp/UE23CS
-git clone https://github.com/polarhive/knowledge --depth=1
+hugo; rm -rf knowledge UE23CS /tmp/UE23CS
+git clone --depth=1 https://github.com/polarhive/knowledge
 git clone --depth=1 https://github.com/polarhive/UE23CS /tmp/UE23CS
-mv /tmp/UE23CS/src knowledge/quartz/content/uni/
-mv /tmp/UE23CS/README.md knowledge/quartz/content/uni/index.md
+mv /tmp/UE23CS/src knowledge/wiki/content/uni/
 cd knowledge/quartz || exit
 npm i; npx quartz build
 mv -f public ../../public/wiki
