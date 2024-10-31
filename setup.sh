fb92508105b7284dc7936fa4c62b6f45b9cf0e1d
Nathan Paul <mail@polarhive.net>
2024-10-31 20:47:33 +0530

    meta: CI for resume

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 hugo; rm -rf knowledge UE23CS /tmp/UE23CS
+curl -L -o public/assets/résumé.pdf https://raw.githubusercontent.com/polarhive/resume/refs/heads/main/r%C3%A9sum%C3%A9.pdf
 git clone --depth=1 https://github.com/polarhive/knowledge
 git clone --depth=1 https://github.com/polarhive/UE23CS /tmp/UE23CS
 mv /tmp/UE23CS/src knowledge/wiki/content/uni/
