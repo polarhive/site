df80ecb9d4431d9f80f92bc59127a103823b7a85
Nathan Paul <mail@polarhive.net>
2024-12-14 15:59:42 +0530

    post: obsidian

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-hugo; rm -rf knowledge UE23CS /tmp/UE23CS
+hugo; rm -rf knowledge UE23CS /tmp/UE23CS /tmp/ideas
 curl -L -o public/assets/résumé.pdf https://raw.githubusercontent.com/polarhive/resume/refs/heads/main/r%C3%A9sum%C3%A9.pdf
 git clone --depth=1 https://github.com/polarhive/knowledge
 git clone --depth=1 https://github.com/polarhive/UE23CS /tmp/UE23CS
+git clone --depth=1 https://github.com/polarhive/ideas /tmp/ideas
+mv /tmp/ideas knowledge/wiki/content/ideas/
 mv /tmp/UE23CS/src knowledge/wiki/content/uni/
 cd knowledge/wiki || exit
 npm i; npx quartz build
