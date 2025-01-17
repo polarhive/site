e101766ec168be53a9919a5eef4f699915e11ba0
Nathan Paul <mail@polarhive.net>
2025-01-18 02:28:43 +0530

    chore: update script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 hugo; rm -rf knowledge UE23CS /tmp/UE23CS /tmp/ideas
 curl -L -o public/assets/résumé.pdf https://raw.githubusercontent.com/polarhive/resume/refs/heads/main/r%C3%A9sum%C3%A9.pdf
-mkdir -p public/gradeshade; curl -L -o public/gradeshade/index.html https://raw.githubusercontent.com/polarhive/gradeshade/refs/heads/main/index.html
-git clone --depth=1 https://github.com/polarhive/knowledge
-git clone --depth=1 https://github.com/polarhive/UE23CS /tmp/UE23CS
-git clone --depth=1 https://github.com/polarhive/ideas /tmp/ideas
+git clone --depth=1 https://codeberg.org/polarhive/knowledge
+git clone --depth=1 https://codeberg.org/polarhive/UE23CS /tmp/UE23CS
+git clone --depth=1 https://codeberg.org/polarhive/ideas /tmp/ideas
 mv /tmp/ideas knowledge/wiki/content/ideas/
 mv /tmp/UE23CS/src knowledge/wiki/content/uni/
 cd knowledge/wiki || exit
